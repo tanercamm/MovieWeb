ee667d6f5de1ead8819764404b094f742a7cc1e3
tanercamm <225260023@firat.edu.tr>
2024-03-12 13:14:25 +0300

    Add Model properties

---
+++ MovieWeb/Models/HomePageViewModel.cs
@@ -0,0 +1,9 @@
+﻿using MovieWeb.Entity;
+
+namespace MovieWeb.Models
+{
+	public class HomePageViewModel
+	{
+        public List<Movie> PopularMovies { get; set; }
+    }
+}
---
+++ MovieWeb/Models/MoviesViewModel.cs
@@ -0,0 +1,9 @@
+﻿using MovieWeb.Entity;
+
+namespace MovieWeb.Models
+{
+	public class MoviesViewModel
+	{
+        public List<Movie> Movies { get; set; }
+    }
+}
