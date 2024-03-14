4a01f9c25fdb9c1128044a504ce8b64a07f0750f
tanercamm <225260023@firat.edu.tr>
2024-03-14 22:35:09 +0300

    Add PersonsController And View

--- MovieWeb/Controllers/PersonsController.cs
+++ MovieWeb/Controllers/PersonsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MovieWeb.Data;
 using MovieWeb.Entity;
+using MovieWeb.Models;
 
 namespace MovieWeb.Controllers
 {
@@ -21,25 +22,120 @@ namespace MovieWeb.Controllers
 		[HttpGet]
 		public IActionResult PersonCreate()
 		{
-			return View();
+			return View(new PersonCreateViewModel());
 		}
 
 		[HttpPost]
-		public IActionResult PersonCreate(Person model)
+		public IActionResult PersonCreate(PersonCreateViewModel model)
 		{
-			_context.Persons.Add(model);
-			return RedirectToAction("PersonList","People");
+			if (ModelState.IsValid)
+			{
+				var person = new Person
+				{
+					Name = model.Name,
+					Biography = model.Biography,
+					Imdb = model.Imdb,
+					PlaceOfBirth = model.PlaceOfBirth,
+					ImageUrl = "icon.jpg"
+				};
+
+				_context.Persons.Add(person);
+				_context.SaveChanges();
+
+				return RedirectToAction("PersonList");
+			}
+			return View(model);
 		}
+
 
 		public IActionResult PersonList()
 		{
 			var people = _context.Persons.ToList();
-			return View(people);
+			var viewModel = new PersonsViewModel
+			{
+				Persons = people
+			};
+			return View(viewModel);
 		}
 
+		[HttpPost]
+		public IActionResult PersonDelete(int personId)
+		{
+			var entity = _context.Persons.Find(personId);
 
+			if (entity != null)
+			{
+				_context.Persons.Remove(entity);
+				_context.SaveChanges();
+			}
+			return RedirectToAction("PersonList");
+		}
 
+		[HttpGet]
+		public IActionResult PersonUpdate(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
+			var entity = _context.Persons.Select(p => new PersonEditViewModel
+			{
+				PersonId = p.PersonId,
+				Name = p.Name,
+				Biography = p.Biography,
+				Imdb = p.Imdb,
+				ImageUrl = p.ImageUrl,
+				PlaceOfBirth = p.PlaceOfBirth
+			}).FirstOrDefault(p => p.PersonId == id);
+
+			if (entity == null)
+			{
+				return NotFound();
+			}
+			return View(entity);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> PersonUpdate(PersonEditViewModel model, IFormFile file)
+		{
+			if (ModelState.IsValid)
+			{
+				var entity = _context.Persons.FirstOrDefault(g => g.PersonId == model.PersonId);
+
+				if(entity == null)
+				{
+					return NotFound();
+				}
+
+				entity.Name = model.Name;
+				entity.Biography = model.Biography;
+				entity.Imdb = model.Imdb;
+				entity.PlaceOfBirth = model.PlaceOfBirth;
+
+				if (file != null)
+				{
+					var extension = Path.GetExtension(file.FileName);  // .jpg , .png  uzantılarını aldık
+					var fileName = Path.GetFileName(file.FileName); // Dosya adı uzantısı ile birlikte
+					var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\persons", fileName);
+					entity.ImageUrl = fileName;
+					using (var stream = new FileStream(path, FileMode.Create))
+					{
+						await file.CopyToAsync(stream);
+					}
+				}
+				else // yeni dosya yüklenmemişse, mevcut resmi tanımlarız
+				{
+					entity.ImageUrl = model.ImageUrl;
+				}
+
+				_context.SaveChanges();
+
+				return RedirectToAction("PersonList");
+			}
+
+			return View(model);
+		}
 
 	}
 }
--- MovieWeb/Models/PersonsViewModel.cs
+++ MovieWeb/Models/PersonsViewModel.cs
@@ -16,18 +16,43 @@ namespace MovieWeb.Models
 		public string Name { get; set; }
 
 		[Required]
-		[StringLength(3000, MinimumLength = 100, ErrorMessage = "Film açıklaması için 10-3000 karakter girmelisiniz!")]
+		[StringLength(3000, MinimumLength = 100, ErrorMessage = "Film açıklaması için 10-3000 karakter  aralığında girmelisiniz!")]
 		public string Biography { get; set; }
 
 		[Required]
 		[DataType(DataType.Url)]
 		public string Imdb { get; set; }
 
-
+		[Display(Name = "City ​​of Birth")]
 		[Required]
-		[DataType(DataType.DateTime)]
+		[StringLength(25, MinimumLength = 2, ErrorMessage = "Doğum yeri için 2-25 karakter aralığında girmelisiniz!")]
 		public string PlaceOfBirth { get; set; }
 	}
+
+	public class PersonEditViewModel
+	{
+        public int PersonId { get; set; }
+
+        [Required]
+		[StringLength(50, MinimumLength = 6, ErrorMessage = "İsim için 6-50 karakter aralığında girmelisiniz!")]
+		public string Name { get; set; }
+
+		[Required]
+		[StringLength(3000, MinimumLength = 100, ErrorMessage = "Film açıklaması için 100-3000 karakter  aralığında girmelisiniz!")]
+		public string Biography { get; set; }
+
+		[Required]
+		[DataType(DataType.Url)]
+		public string Imdb { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        [Display(Name = "City ​​of Birth")]
+		[Required]
+		[StringLength(25, MinimumLength = 2, ErrorMessage = "Doğum yeri için 2-25 karakter aralığında girmelisiniz!")]
+		public string PlaceOfBirth { get; set; }
+	}
+
 
 }
 
